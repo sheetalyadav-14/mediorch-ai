cdec6ecebece9f90e444793164034974f505f62a
Sheetal Yadav <ysheetal740@gmail.com>
2026-04-08 17:33:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-pro")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 from fastapi import FastAPI
 from pydantic import BaseModel
