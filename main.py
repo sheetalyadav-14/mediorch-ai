9bf04c4a6de7d108b820499faa9a73f6a039e311
Sheetal Yadav <ysheetal740@gmail.com>
2026-04-08 19:10:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -135,11 +135,3 @@ def chat(query: Query):
         return {"error": str(e)}
 
 # ------------------ RUN ------------------
-
-if __name__ == "__main__":
-    import uvicorn
-    import os
-
-    port = int(os.environ.get("PORT", 8080))
-
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
